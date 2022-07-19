66ce5d67087aaa0a6edc8ce44f4bab85d149916f
Kuvaev-dev <likeaboss13371@gmail.com>
2022-07-19 16:02:39 +0300

    New Authorization Logic Added

--- app/src/main/java/kuvaev/mainapp/eatit/SignInActivity.java
+++ app/src/main/java/kuvaev/mainapp/eatit/SignInActivity.java
@@ -1,12 +1,21 @@
 package kuvaev.mainapp.eatit;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -18,56 +27,213 @@ import com.rengwuxian.materialedittext.MaterialEditText;
 
 import java.util.Objects;
 
+import io.paperdb.Paper;
 import kuvaev.mainapp.eatit.Common.Common;
 import kuvaev.mainapp.eatit.Model.User;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class SignInActivity extends AppCompatActivity {
-    MaterialEditText editPhone, editPassword;
+    MaterialEditText edtPhone, edtPassword;
+    CheckBox ckbRemember;
     Button btnSignIn;
+    TextView txtForgotPwd;
+    LinearLayout facebookSignIn;
+
+    FirebaseDatabase database;
+    DatabaseReference table_user;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Note: add this code before setContentView method
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/restaurant_font.otf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+
         setContentView(R.layout.activity_sign_in);
 
-        editPassword = findViewById(R.id.editPassword);
-        editPhone = findViewById(R.id.editPhone);
-        btnSignIn = findViewById(R.id.btnSignIn);
+        edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
+        edtPassword = (MaterialEditText)findViewById(R.id.edtPassword);
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+        ckbRemember = (CheckBox)findViewById(R.id.ckbRemember);
+        txtForgotPwd = (TextView)findViewById(R.id.txtForgotPwd);
+        facebookSignIn = (LinearLayout)findViewById(R.id.SignInFaceBook);
 
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference table_user = database.getReference("User");
+        //Fix showing characters in MaterialEditText password
+        Typeface typeface = Typeface.DEFAULT;
+        edtPassword.setTypeface(typeface);
 
-        btnSignIn.setOnClickListener(view -> {
-            ProgressDialog mDialog = new ProgressDialog(SignInActivity.this);
-            mDialog.setMessage("Please wait...");
-            mDialog.show();
+        //Init Paper
+        Paper.init(this);
 
+        //Init Firebase
+        database = FirebaseDatabase.getInstance();
+        table_user = database.getReference("User");
+
+        txtForgotPwd.setOnClickListener(v -> showForgotPwdDialog());
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (Common.isConnectionToInternet(SignInActivity.this)) {
+                    //Save user & password
+                    if (ckbRemember.isChecked()){
+                        Paper.book().write(Common.USER_KEY , Objects.requireNonNull(edtPhone.getText()).toString());
+                        Paper.book().write(Common.PWD_KEY , Objects.requireNonNull(edtPassword.getText()).toString());
+                    }
+
+                    final AlertDialog dialog = new SpotsDialog(SignInActivity.this);
+                    dialog.show();
+                    dialog.setMessage("Please waiting...");
+
+
+                    table_user.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                            //Check if user not exist in database
+                            if (dataSnapshot.child(Objects.requireNonNull(edtPhone.getText()).toString()).exists()) {
+
+                                //Get user information
+                                dialog.dismiss();
+                                User user = dataSnapshot.child(edtPhone.getText().toString()).getValue(User.class);
+                                assert user != null;
+                                user.setPhone(edtPhone.getText().toString()); //set Phone
+
+                                if (user.getPassword().equals(Objects.requireNonNull(edtPassword.getText()).toString())) {
+                                    Intent homeIntent = new Intent(SignInActivity.this, Home.class);
+                                    Common.currentUser = user;
+                                    startActivity(homeIntent);
+                                    finish();
+
+                                    Toast.makeText(SignInActivity.this, "Sign In Successfully !", Toast.LENGTH_SHORT).show();
+
+                                    table_user.removeEventListener(this);
+                                }
+                                else {
+                                    Toast.makeText(SignInActivity.this, "Wrong Password !!!", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                            else {
+                                dialog.dismiss();
+                                Toast.makeText(SignInActivity.this, "User not exist in Database", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+                else {
+                    Toast.makeText(SignInActivity.this, "Please check your connection !!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        facebookSignIn.setOnClickListener(v -> {
+            if (AccountKit.getCurrentAccessToken() == null){   //he doesn't have an account in Facebook Account kit
+                Toast.makeText(SignInActivity.this, "You don't have an account , Please Sign up by your facebook account", Toast.LENGTH_SHORT).show();
+                finish();
+            } else { //he have an account
+
+                final AlertDialog dialog = new SpotsDialog(SignInActivity.this);
+                dialog.show();
+                dialog.setCancelable(false);
+                dialog.setMessage("Please waiting...");
+
+
+                AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                    @Override
+                    public void onSuccess(final Account account) {
+
+                        table_user.child(account.getPhoneNumber().toString())
+                                .addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                        User localUser = dataSnapshot.getValue(User.class);
+
+                                        Intent homeIntent = new Intent(SignInActivity.this, Home.class);
+                                        Common.currentUser = localUser;
+                                        Common.currentUser.setPhone(account.getPhoneNumber().toString());
+                                        startActivity(homeIntent);
+
+                                        dialog.dismiss();
+                                        finish();
+
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onError(AccountKitError accountKitError) {
+
+                    }
+                });
+            }
+
+        });
+
+    }
+
+    private void showForgotPwdDialog() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this)
+                .setTitle("Forgot Password")
+                .setMessage("Enter your secure code")
+                .setIcon(R.drawable.ic_security_black_24dp);
+
+        View forgot_view = getLayoutInflater().inflate(R.layout.layout_forgot_password , null);
+        builder.setView(forgot_view);
+
+        final MaterialEditText edtPhone = (MaterialEditText)forgot_view.findViewById(R.id.edtPhone);
+        final MaterialEditText edtSecureCode = (MaterialEditText)forgot_view.findViewById(R.id.edtSecureCode);
+
+        builder.setPositiveButton("YES", (dialog, which) -> {
+            //Check if user available
             table_user.addValueEventListener(new ValueEventListener() {
                 @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if (snapshot.child(Objects.requireNonNull(editPhone.getText()).toString()).exists()) {
-                        mDialog.dismiss();
-                        User user = snapshot.child(editPhone.getText().toString()).getValue(User.class);
-                        assert user != null;
-                        if (user.getPassword().equals(Objects.requireNonNull(editPassword.getText()).toString())) {
-                            Intent intent = new Intent(SignInActivity.this, Home.class);
-                            Common.currentUser = user;
-                            startActivity(intent);
-                            finish();
-                        } else {
-                            Toast.makeText(SignInActivity.this, "Wrong Password!", Toast.LENGTH_SHORT).show();
-                        }
-                    } else {
-                        mDialog.dismiss();
-                        Toast.makeText(SignInActivity.this, "User not exist!", Toast.LENGTH_SHORT).show();
-                    }
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    User user = dataSnapshot.child(Objects.requireNonNull(edtPhone.getText()).toString())
+                            .getValue(User.class);
+
+                    assert user != null;
+                    if (user.getSecureCode().equals(Objects.requireNonNull(edtSecureCode.getText()).toString()))
+                        Toast.makeText(SignInActivity.this, "Your password : " + user.getPassword(), Toast.LENGTH_LONG).show();
+                    else
+                        Toast.makeText(SignInActivity.this, "Wrong secure code !", Toast.LENGTH_SHORT).show();
                 }
 
                 @Override
-                public void onCancelled(@NonNull DatabaseError error) {
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
                 }
             });
         });
+
+        builder.setNegativeButton("NO", (dialog, which) -> {
+
+        });
+
+        builder.show();
     }
 }
--- app/src/main/java/kuvaev/mainapp/eatit/SignUpActivity.java
+++ app/src/main/java/kuvaev/mainapp/eatit/SignUpActivity.java
@@ -1,13 +1,23 @@
 package kuvaev.mainapp.eatit;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,53 +27,225 @@ import com.rengwuxian.materialedittext.MaterialEditText;
 
 import java.util.Objects;
 
+import kuvaev.mainapp.eatit.Common.Common;
 import kuvaev.mainapp.eatit.Model.User;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class SignUpActivity extends AppCompatActivity {
-    MaterialEditText editPhone, editName, editPassword;
+    private static final int REQUEST_CODE = 7171 ;
+
+    MaterialEditText edtPhone , edtName , edtPassword , edtSecureCode;
     Button btnSignUp;
+    LinearLayout facebookSignUp;
+
+    AlertDialog waitingDialog;
+
+    FirebaseDatabase database;
+    DatabaseReference table_user;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Note: add this code before setContentView method
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/restaurant_font.otf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+
         setContentView(R.layout.activity_sign_up);
 
-        editName = findViewById(R.id.editName);
-        editPassword = findViewById(R.id.editPassword);
-        editPhone = findViewById(R.id.editPhone);
-        btnSignUp = findViewById(R.id.btnSignUp);
+        edtPhone = (MaterialEditText)findViewById(R.id.edtPhone);
+        edtName = (MaterialEditText)findViewById(R.id.edtName);
+        edtPassword = (MaterialEditText)findViewById(R.id.edtPassword);
+        edtSecureCode = (MaterialEditText)findViewById(R.id.edtSecureCode);
+        btnSignUp = (Button)findViewById(R.id.btnSignUp);
+        facebookSignUp = (LinearLayout)findViewById(R.id.SignUpFaceBook);
 
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference table_user = database.getReference("User");
+        //Fix showing characters in MaterialEditText password
+        Typeface typeface = Typeface.DEFAULT;
+        edtPassword.setTypeface(typeface);
 
-        btnSignUp.setOnClickListener(view -> {
-            ProgressDialog mDialog = new ProgressDialog(SignUpActivity.this);
-            mDialog.setMessage("Please wait...");
-            mDialog.show();
+        //Init Firebase
+        database = FirebaseDatabase.getInstance();
+        table_user = database.getReference("User");
 
-            table_user.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if (snapshot.child(Objects.requireNonNull(editPhone.getText()).toString()).exists()) {
-                        mDialog.dismiss();
-                        Toast.makeText(SignUpActivity.this, "Phone number already exist!", Toast.LENGTH_SHORT).show();
-                    } else {
-                        mDialog.dismiss();
-                        User user = new User(
-                                Objects.requireNonNull(editName.getText()).toString(),
-                                Objects.requireNonNull(editPassword.getText()).toString()
-                        );
-                        table_user.child(editPhone.getText().toString()).setValue(user);
-                        Toast.makeText(SignUpActivity.this, "Sign up successfully!", Toast.LENGTH_SHORT).show();
-                        finish();
+        btnSignUp.setOnClickListener(v -> {
+            if (Common.isConnectionToInternet(SignUpActivity.this)) {
+                final AlertDialog dialog = new SpotsDialog(SignUpActivity.this);
+                dialog.setMessage("Please waiting...");
+                dialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        // Check if already user phone
+                        if (dataSnapshot.child(Objects.requireNonNull(edtPhone.getText()).toString()).exists()){
+                            dialog.dismiss();
+                            Toast.makeText(SignUpActivity.this, "Phone Number already register", Toast.LENGTH_SHORT).show();
+                        } else {
+                            dialog.dismiss();
+
+                            User user = new User(Objects.requireNonNull(edtName.getText()).toString() ,
+                                    Objects.requireNonNull(edtPassword.getText()).toString(),
+                                    Objects.requireNonNull(edtSecureCode.getText()).toString());
+                            user.setBalance(0.0);
+                            table_user.child(edtPhone.getText().toString()).setValue(user);
+
+                            Intent intentHome = new Intent(SignUpActivity.this , Home.class);
+                            Common.currentUser = user;
+                            startActivity(intentHome);
+                            finish();
+
+                            Toast.makeText(SignUpActivity.this, "Sign Up Successfully !", Toast.LENGTH_SHORT).show();
+                        }
                     }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            } else {
+                Toast.makeText(SignUpActivity.this, "Please check your connection !!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        facebookSignUp.setOnClickListener(v -> {
+            if (Common.isConnectionToInternet(getApplicationContext())){
+                if (AccountKit.getCurrentAccessToken() != null){   //he have an account in Facebook Account kit
+                    Toast.makeText(SignUpActivity.this, "You already have an account ,just sign in by FaceBook account", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                else { //he doesn't have an account
+
+                    Intent intent = new Intent(SignUpActivity.this, AccountKitActivity.class);
+                    AccountKitConfiguration.AccountKitConfigurationBuilder builder =
+                            new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE,
+                                    AccountKitActivity.ResponseType.TOKEN);
+                    intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION , builder.build());
+                    startActivityForResult(intent , REQUEST_CODE);
+                }
+
+            }
+            else {
+                Toast.makeText(SignUpActivity.this, "Please check your connection !!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE){
+            AccountKitLoginResult result = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            if (result.getError() != null){
+                Toast.makeText(this, result.getError().getErrorType().toString(), Toast.LENGTH_SHORT).show();
+            }
+            else if (result.wasCancelled()){
+                Toast.makeText(this, "Canceled !!!", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                checkUserAccount(result);
+            }
+        }
+    }
+
+    private void checkUserAccount(final AccountKitLoginResult result) {
+        if (result.getAccessToken() != null){  //Sign up is completed Successfully
+            //Show Dialog
+            waitingDialog = new SpotsDialog(this);
+            waitingDialog.show();
+            waitingDialog.setMessage("Please waiting...");
+            waitingDialog.setCancelable(false);
+
+            //Get current Phone
+            AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                @Override
+                public void onSuccess(Account account) {
+                    final String userPhone = account.getPhoneNumber().toString();
+
+                    //Check if the user exists in FireBase database' table of User
+                    table_user.orderByKey().equalTo(userPhone)
+                            .addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    if (!dataSnapshot.child(userPhone).exists()){  //If not exists
+                                        //We will create new user and login
+                                        addNewUser(userPhone);
+                                    }
+                                    else {  //If exists
+                                        waitingDialog.dismiss();
+                                        Toast.makeText(SignUpActivity.this, "You have already account ,just sign in by FaceBook account", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
                 }
 
                 @Override
-                public void onCancelled(@NonNull DatabaseError error) {
-
+                public void onError(AccountKitError accountKitError) {
+                    waitingDialog.dismiss();
+                    Toast.makeText(SignUpActivity.this, accountKitError.getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
-        });
+        }
+    }
+
+    private void addNewUser(final String phone) {
+        String name = "___", password = "___", secureCode = "___";
+
+        User newUser = new User();
+        newUser.setName(name);
+        newUser.setPassword(password);
+        newUser.setSecureCode(secureCode);
+        newUser.setIsStaff("false");
+        newUser.setBalance(0.0);
+
+        // Add to FireBase database
+        table_user.child(phone)
+            .setValue(newUser)
+            .addOnCompleteListener(task -> {
+                if (task.isSuccessful())
+                    Toast.makeText(SignUpActivity.this, "User register successfully !!!", Toast.LENGTH_SHORT).show();
+
+                loginHome(phone);
+            })
+            .addOnFailureListener(e -> Toast.makeText(SignUp.this, e.getMessage(), Toast.LENGTH_SHORT).show());
+    }
+
+    private void loginHome(final String phone) {
+        table_user.child(phone)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        User localUser = dataSnapshot.getValue(User.class);
+
+                        Intent homeIntent = new Intent(SignUp.this, Home.class);
+                        Common.currentUser = localUser;
+                        assert Common.currentUser != null;
+                        Common.currentUser.setPhone(phone);
+                        startActivity(homeIntent);
+
+                        waitingDialog.dismiss();
+                        finish();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 }
