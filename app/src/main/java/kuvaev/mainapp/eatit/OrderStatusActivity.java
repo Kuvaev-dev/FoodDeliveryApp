09fa7c5c0c2cb429765a7b982ad3303705ab00ee
Kuvaev-dev <likeaboss13371@gmail.com>
2022-07-20 13:05:12 +0300

    Order Status And Adapters Logic Added

--- app/src/main/java/kuvaev/mainapp/eatit/OrderStatusActivity.java
+++ app/src/main/java/kuvaev/mainapp/eatit/OrderStatusActivity.java
@@ -1,13 +1,123 @@
 package kuvaev.mainapp.eatit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import kuvaev.mainapp.eatit.Common.Common;
+import kuvaev.mainapp.eatit.Interface.ItemClickListener;
+import kuvaev.mainapp.eatit.Model.Request;
+import kuvaev.mainapp.eatit.ViewHolder.OrderViewHolder;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class OrderStatusActivity extends AppCompatActivity {
+    public RecyclerView recyclerView;
+
+    FirebaseDatabase database;
+    DatabaseReference requests;
+    FirebaseRecyclerAdapter<Request, OrderViewHolder> adapter;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Note: add this code before setContentView method
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/restaurant_font.otf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+
         setContentView(R.layout.activity_order_status);
+
+        // FireBase
+        database = FirebaseDatabase.getInstance();
+        requests = database.getReference("Requests");
+
+        recyclerView = (RecyclerView)findViewById(R.id.listOrders);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        // If we start OrderStatus activity from Home Activity
+        // We will not put any exta , so we just loadOrder by phone  from Common
+        if (Common.isConnectionToInternet(this)){
+            if (getIntent().getStringExtra("userPhone").equals("-1"))
+                loadOrders(Common.currentUser.getPhone());
+            else {
+                if (getIntent().getStringExtra("userPhone") == null)
+                    loadOrders(Common.currentUser.getPhone());
+                else
+                    loadOrders(getIntent().getStringExtra("userPhone"));
+            }
+        }
+        else
+            Toast.makeText(this, "Please check your connection", Toast.LENGTH_SHORT).show();
     }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void loadOrders(String phone) {
+        adapter = new FirebaseRecyclerAdapter<Request, OrderViewHolder>(
+                Request.class,
+                R.layout.layout_order,
+                OrderViewHolder.class,
+                requests.orderByChild("phone").equalTo(phone)
+        ) {
+            @SuppressLint("NotifyDataSetChanged")
+            @Override
+            private void populateViewHolder(OrderViewHolder viewHolder, Request model, final int position) {
+                viewHolder.txtOrderId.setText(adapter.getRef(position).getKey());
+                viewHolder.txtOrderStatus.setText(Common.convertCodeToStatus(model.getStatus()));
+                viewHolder.txtOrderAddress.setText(model.getAddress());
+                viewHolder.txtOrderPhone.setText(model.getPhone());
+
+                viewHolder.btn_delete.setOnClickListener(v -> {
+                    if (Common.isConnectionToInternet(getApplicationContext())){
+                        if (adapter.getItem(position).getStatus().equals("0")){
+                            deleteOrder(adapter.getRef(position).getKey());
+                            adapter.notifyDataSetChanged();
+                            recyclerView.setAdapter(adapter);
+                        }
+                        else
+                            Toast.makeText(OrderStatusActivity.this, "You can't delete this order !", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                        Toast.makeText(OrderStatusActivity.this, "Please check your connection !", Toast.LENGTH_SHORT).show();
+                });
+
+                viewHolder.setItemClickListener((view, position1, isLongClick) -> {
+                    Common.currentKey = adapter.getRef(position1).getKey();
+                    startActivity(new Intent(OrderStatusActivity.this, TrackingOrderActivity.class));
+                });
+            }
+        };
+        recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    private void deleteOrder(final String key) {
+        requests.child(key)
+                .removeValue().addOnSuccessListener(aVoid -> Toast.makeText(OrderStatusActivity.this, new StringBuilder("Order ")
+                    .append(key)
+                    .append(" has been deleted !"), Toast.LENGTH_SHORT).show())
+                    .addOnFailureListener(e -> Toast.makeText(OrderStatusActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show());
+    }
+
 }
--- app/src/main/java/kuvaev/mainapp/eatit/ViewHolder/CartAdapter.java
+++ app/src/main/java/kuvaev/mainapp/eatit/ViewHolder/CartAdapter.java
@@ -1,4 +1,113 @@
 package kuvaev.mainapp.eatit.ViewHolder;
 
-public class CartAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.text.NumberFormat;
+import java.util.List;
+import java.util.Locale;
+
+import kuvaev.mainapp.eatit.CartActivity;
+import kuvaev.mainapp.eatit.Common.Common;
+import kuvaev.mainapp.eatit.Database.Database;
+import kuvaev.mainapp.eatit.Model.Food;
+import kuvaev.mainapp.eatit.Model.Order;
+
+public class CartAdapter extends RecyclerView.Adapter<CartViewHolder> {
+    private final List<Order> listData;
+    private final CartActivity cart;
+
+    private String foodImage;
+    Food currentFood;
+
+    public CartAdapter(List<Order> listData, CartActivity cart) {
+        this.listData = listData;
+        this.cart = cart;
+    }
+
+    @NonNull
+    @Override
+    public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(cart).inflate(R.layout.cart_layout, parent, false);
+        return new CartViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final CartViewHolder holder) {
+        holder.txt_cart_name.setText(listData.get(holder.getBindingAdapterPosition()).getProductName());
+
+        Picasso.with(cart.getBaseContext())
+                .load(listData.get(holder.getBindingAdapterPosition()).getImage())
+                .placeholder(android.R.color.holo_green_dark)
+                .resize(70 , 70)
+                .centerCrop()
+                .into(holder.imgCart, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Toast.makeText(cart, "SUCCESS", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+                        Toast.makeText(cart, "ERROR: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        holder.btn_quantity.setNumber(listData.get(holder.getBindingAdapterPosition()).getQuantity());
+        holder.btn_quantity.setOnValueChangeListener(new ElegantNumberButton.OnValueChangeListener() {
+            @Override
+            public void onValueChange(ElegantNumberButton view, int oldValue, int newValue) {
+
+                Order order = listData.get(holder.getBindingAdapterPosition());
+                order.setQuantity(String.valueOf(newValue));
+                new Database(cart).updateCart(order);
+
+                // Update txtTotalPrice
+                // Calculate total price
+                int total = 0;
+                List<Order> orders = new Database(cart).getCarts(Common.currentUser.getPhone());
+
+                for (Order item : orders)
+                    total += (Integer.parseInt(order.getPrice()) * Integer.parseInt(item.getQuantity()));
+
+                Locale locale = new Locale("en" , "US");
+                NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
+
+                cart.txtTotalPrice.setText(fmt.format(total));
+            }
+        });
+
+        Locale locale = new Locale("en" , "US");
+        NumberFormat fmt = NumberFormat.getCurrencyInstance(locale);
+        int price = Integer.parseInt(listData.get(holder.getBindingAdapterPosition()).getPrice()) *
+                    Integer.parseInt(listData.get(holder.getBindingAdapterPosition()).getQuantity());
+        holder.txt_price.setText(fmt.format(price));
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    public Order getItem(int position){
+        return listData.get(position);
+    }
+
+    public void removeItem(int position){
+        listData.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void restoreItem(Order item, int position){
+        listData.add(position , item);
+        notifyItemInserted(position);
+    }
 }
